16c0988f769cf1dbffcf3d630cc33dbc6471d318
TTRademacher <rademacher.tim@gmail.com>
2020-03-04 12:46:59 -0700

    Started the statistical analysis of the mixed effects model.

--- respirationFigures.R
+++ respirationFigures.R
@@ -6,6 +6,9 @@
 # set colour scheme for control, girdled, compressed, double compressed and chilled
 #----------------------------------------------------------------------------------------
 colours <- c ('#91b9a4','#C0334D','#F18904','#5C4A72','#23345C')
+colourPine  <- '#66c2a5'
+colourMaple <- '#fc8d62'
+colourOak   <- '#8da0cb'
 
 # Function to add opacity to a colour
 #----------------------------------------------------------------------------------------
@@ -22,9 +25,130 @@ girdledTrees <- c ( 5, 11, 15, 16, 19, 23, 29, 35, 39, 40)
 compresTrees <- c (10, 12, 13, 17, 20, 21, 28, 32, 33, 38)
 douCompTrees <- c ( 2,  8, 14, 22, 24, 25, 26, 27, 34, 37)
 
-ALPHA  <- 0.5 # add transparency to better see all symbols
+ALPHA  <- 0.3 # add transparency to better see all symbols
 
-# plot stem CO2 efflux for each tree over time
+# Read processed respiration data
 #----------------------------------------------------------------------------------------
+source ('/home/tim/projects/PlantGrowth/stemCO2Efflux/readProcessedRespData.R')
+
+# Plot the respiration rate of control tree white pines versus temperature
+#----------------------------------------------------------------------------------------
+plot (respData [['airt.C']] [respData [['treatment']] == 1 & 
+                               respData [['species']] == 'Pinus strobus'],
+      respData [['flux']] [respData [['treatment']] == 1 & 
+                             respData [['species']] == 'Pinus strobus'],
+      ylab = 'stem CO2 efflux', xlab = 'temperature (degC)', xlim = c (0, 30), 
+      ylim = c (0, 26), las = 1, pch = 19, col = addOpacity (colourPine, ALPHA))
+points (respData [['airt.C']] [respData [['treatment']] == 1 & 
+                               respData [['species']] == 'Pinus strobus' &
+                               respData [['study']]  == 'Exp2017'],
+        respData [['flux']] [respData [['treatment']] == 1 & 
+                             respData [['species']] == 'Pinus strobus' &
+                             respData [['study']] == 'Exp2017'],
+        pch = 19, col = addOpacity ('#106470', ALPHA))
+
+# Fit a temperature response curve to all control group white pines
+#----------------------------------------------------------------------------------------
+
+
+# Fit a temperature response curve to Exp2017 control group white pines
+#----------------------------------------------------------------------------------------
+
+
+# Plot stem CO2 efflux for each Exp2017 tree over time
+#----------------------------------------------------------------------------------------
+par (mar = c (5, 5, 1, 1))
+
+plot (respData [['timestamp']] [respData [['tree']] == 1 & respData [['chamber']] == 1 & 
+                                  respData [['study']] == 'Exp2017'],
+      respData [['flux']] [respData [['tree']] == 1 & respData [['chamber']] == 1 & 
+                             respData [['study']] == 'Exp2017'], col = 'white',
+      typ = 'l', xlab = 'date', ylab = 'respiration', las = 1, ylim = c (0, 8.7))
+sapply (controlTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                     respData [['study']] == 'Exp2017']))
+sapply (girdledTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 2))
+sapply (compresTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 1))
+sapply (compresTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 2))
+sapply (girdledTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 1))
+sapply (douCompTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 2))
+sapply (douCompTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 3))
+sapply (douCompTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 3 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 3 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 1))
+
+
+plot (respData [['timestamp']] [respData [['tree']] == 1 & respData [['chamber']] == 1 & 
+                                  respData [['study']] == 'Exp2017'],
+      respData [['flux']] [respData [['tree']] == 1 & respData [['chamber']] == 1 & 
+                           respData [['study']] == 'Exp2017'], col = 'white',
+      typ = 'l', xlab = 'date', ylab = 'respiration', las = 1, ylim = c (0, 8.7),
+      xlim = c (as.POSIXct ('2017-06-15'), as.POSIXct ('2017-11-15')))
+sapply (controlTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                   respData [['study']] == 'Exp2017'],
+       respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                              respData [['study']] == 'Exp2017']))
+sapply (girdledTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 2))
+sapply (girdledTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 1))
+sapply (compresTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 2))
+sapply (compresTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 1))
+sapply (douCompTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 1 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 2))
+sapply (douCompTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 2 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 3))
+sapply (douCompTrees, function (iTree) lines (respData [['timestamp']] [respData [['tree']] == iTree & respData [['chamber']] == 3 & 
+                                                                          respData [['study']] == 'Exp2017'],
+                                              respData [['flux']] [respData [['tree']] == iTree & respData [['chamber']] == 3 & 
+                                                                     respData [['study']] == 'Exp2017'],
+                                              lty = 1))
 
 #========================================================================================
--- respirationStats.R
+++ respirationStats.R
@@ -6,10 +6,33 @@
 # load dependencies
 #----------------------------------------------------------------------------------------
 library ('lme4')
-library ('nlme')
+library ('tidyverse')
+library ('lubridate')
 
-# read respiration data
+# Read processed respiration data
 #----------------------------------------------------------------------------------------
+source ('/home/tim/projects/PlantGrowth/stemCO2Efflux/readProcessedRespData.R')
 
+# Wrangle the data 
+#----------------------------------------------------------------------------------------
+respData [['date']] <- as_date (respData [['timestamp']])
+respData [['treatment']] <- factor (respData [['treatment']], levels = c (4:1))
+respData [['tree']] <- factor (respData [['tree']])
+respData [['height']] <- respData [['chamber']]
+respData [['height']] [respData [['height']] == 1 & respData [['treatment']] == 1] <- 'C'
+respData [['height']] [respData [['height']] == 1 & respData [['treatment']] != 1] <- 'B'
+respData [['height']] [respData [['height']] == 2 & (respData [['treatment']] == 2 | 
+                                                     respData [['treatment']] == 3)] <- 'A'
+respData [['height']] [respData [['height']] == 2 & respData [['treatment']] == 4] <- 'M'
+respData [['height']] [respData [['height']] == 3 & respData [['treatment']] == 4] <- 'A'
+respData [['height']] <- factor (respData [['height']], levels = c ('A','M','B','C'))
 
+# fit mixed effects model with tree as random effect
+#----------------------------------------------------------------------------------------
+M1 <- lmer (formula = flux ~ (1 | tree) + factor (date) + factor (date):treatment:height, 
+            data = filter (respData, study == 'Exp2017'),
+            REML = FALSE)
+summary (M1)
+plot (M1)
+qqnorm (resid (M1))
 #========================================================================================
